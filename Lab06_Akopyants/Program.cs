cdf725e399cbfba1a307260a9569ecf7d153c093
ArtZetD <56314346+ArtZetD@users.noreply.github.com>
2022-11-12 01:50:19 +0300

    Done (maybe)

---
+++ Lab06_Akopyants/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+using System.Globalization;
+
+//CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+
+namespace Lab06_Akopyants
+{
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            SortedSet<string> countries = new SortedSet<string>();
+            List<weather> weathers = new List<weather>();
+            for (; weathers.Count() < 10;)
+            {
+                weathers.Add(new weather());
+               // Thread.Sleep(1000);
+            }
+            foreach (var weather in weathers)
+            {
+                await weather.generate_req_res();
+                if (weather.Country is null || weather.Name is null)
+                {
+                    continue;
+                }
+                else
+                {
+                    weather.printInfo();
+                }
+            }
+            foreach (var weather in weathers)
+            {
+                weather.printInfo();
+            }
+
+            //double average_temp = 0;
+            //foreach (var weather in weathers)
+            //{
+            //    await weather.generate_req_res();
+            //    average_temp += Convert.ToDouble(weather.Temp);
+            //    countries.Add(weather.Country);
+            //}
+            //Console.WriteLine($"Average temperature: {average_temp}");
+
+            var min_max = from weath in weathers
+                          orderby weath.Temp
+                          select weath;
+            Console.WriteLine($"Max: {min_max.Last()}, Min: {min_max.First()}");
+
+            Console.WriteLine($"Countries: {countries.Count}");
+
+            var clear_sky = from weath in weathers
+                            where (weath.Description == "clear sky")
+                            select weath;
+            Console.WriteLine($"First country with clear sky: {clear_sky.First().Country}");
+
+            var rainy = from weath in weathers
+                        where (weath.Description == "rain")
+                        select weath;
+            Console.WriteLine($"First country with rain: {rainy.First().Description}");
+
+            //var few_clouds = from weath in weathers
+            //                 where (weath.Description == "few clouds")
+            //                 select weath;
+            //Console.WriteLine($"First country with few clouds: {few_clouds.First().Description}");
+        }
+    }
+}
---
+++ Lab06_Akopyants/weather.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+namespace Lab06_Akopyants
+{
+    class weather
+    {
+        public weather() // определить широту и долготу
+        {
+            latitude = generate_latitude();
+            longitude = generate_longitude();
+        }
+
+        public string generate_latitude()
+        {
+            string latitude = "";
+            Random random = new Random();
+            int first = -90;
+            int last = 90;
+            latitude = Convert.ToString(random.Next(first, last));
+            return latitude;
+        }
+
+        public string generate_longitude()
+        {
+            string longitude = "";
+            Random random = new Random();
+            int first = -180;
+            int last = 180;
+            longitude = Convert.ToString(random.Next(first, last));
+            return longitude;
+        }
+        public async Task generate_req_res() // запрос и ответ от сервера 
+        {
+            Console.Write($"Generate request");
+            print_dots();
+            HttpClient http_client = new HttpClient();
+            string request = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_key}"; // запрос
+            using HttpResponseMessage serv_response = await http_client.GetAsync(request);
+
+            Console.Write("Waiting for response");
+            print_dots();
+            string response = await serv_response.Content.ReadAsStringAsync(); //  ответ
+            var deserializeObject = JsonConvert.DeserializeObject<dynamic>(response)!;
+            Country = deserializeObject.sys.country;
+            Name = deserializeObject.name;
+            Temp = deserializeObject.main.temp - 273;
+            Description = deserializeObject.weather[0].description;
+        }
+
+
+        public void printInfo()
+        {
+            Thread.Sleep(100);
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Country: {Country}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Temp: {Temp}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Thread.Sleep(100);
+        }
+
+
+        private void print_dots()
+        {
+            for(int i = 0; i < 15; ++i)
+            {
+               // Thread.Sleep(20);
+                Console.Write($".");
+                
+            }
+            Console.WriteLine();
+        }
+
+
+        public int CompareTo(weather other)
+        {
+            if (Temp.CompareTo(other.Temp) != 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private const string API_key = "b92af92b0160a995c820f7d6e5155d4a";
+        private string latitude;
+        private string longitude;
+        private IComparable<weather> comparable;
+
+        public string? Country;
+        public string? Name;
+        public string? Temp;
+        public string? Description;
+    }
+}
